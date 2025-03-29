4ebd45f508101e29730aa055ab1b35bd8e596dfe
Mohit Kumar <mkmohitkmr@gmail.com>
2025-03-30 01:03:53 +0530

    [Graph] Tarjans SCC

--- graph/src/main/java/org/javadsa/graph/App.java
+++ graph/src/main/java/org/javadsa/graph/App.java
@@ -31,7 +31,7 @@ public class App
         edges[4] = edge4; edges[5] = edge5; edges[6] = edge6; edges[7] = edge7;
 
         System.out.println("Adjacency list graph: ");
-        GraphIntf<Integer> graphList = new GraphAdjList<>(nodesCount, nodes, edges, false);
+        GraphIntf<Integer> graphList = new GraphAdjList<>(nodesCount, nodes, edges, true);
         System.out.println("DFS: ");
         graphList.dfs();
         System.out.println("BFS: ");
@@ -62,10 +62,11 @@ public class App
             }
             System.out.println();
         }
+        System.out.println("Number of components in graph: "+graphList.tarjansScc());
 
 
         System.out.println("\n\nAdjacency matrix graph: ");
-        GraphIntf<Integer> graphMat = new GraphAdjMat<>(nodesCount, nodes, edges, false);
+        GraphIntf<Integer> graphMat = new GraphAdjMat<>(nodesCount, nodes, edges, true);
         System.out.println("DFS: ");
         graphMat.dfs();
         System.out.println("BFS: ");
@@ -96,5 +97,7 @@ public class App
             }
             System.out.println();
         }
+
+        System.out.println("Number of components in graph: "+graphMat.tarjansScc());
     }
 }
--- graph/src/main/java/org/javadsa/graph/GraphIntf.java
+++ graph/src/main/java/org/javadsa/graph/GraphIntf.java
@@ -25,8 +25,8 @@ public interface GraphIntf<T> {
     public boolean containsNegativeCycle();
 //    // All source shortest path algorithm
     public Double[][] floydWarshall();
-//    // Detect all strongly connected components
-//    public void tarjansScc();
+    // Detect all strongly connected components
+    public int tarjansScc();
 //    // Detect all strongly connected components
 //    public void kosarajuScc();
 //    // Topological sorting
